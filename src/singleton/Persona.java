002cb58fc28b13cbfe2a28e552f1a717ec5537b1
afernandezfontenla <afernandezfontenla@gmail.com>
2020-05-12 16:36:53 +0200

    Introducción de los comentarios

--- src/singleton/Persona.java
+++ src/singleton/Persona.java
@@ -6,20 +6,24 @@ package singleton;
  */
 public class Persona {
 
+    //Declaración de las variables
     private String nombre;
     private String apellido;
     private int edad;
     private static Persona launica;
 
+    //Creación de un constructor vacío, sin parámetros
     private Persona() {
     }
 
+    //Creación de un constructor con parámetros
     private Persona(String nom, String ape, int edad) {
         this.nombre = nom;
         this.apellido = ape;
         this.edad = edad;
     }
 
+    //Creación del método getInstance
     public static Persona getInstance(){
         if (launica == null){
             launica = new Persona();
@@ -27,29 +31,34 @@ public class Persona {
         return launica;
     }
 
+    //Creación del método getInstancePar
     public static Persona getInstancePar(String nom, String ape, int edad){
         if (launica == null){
             launica = new Persona(nom,ape,edad);
         }
         return launica;
     }
+
+    //Creación del método setNom
     public void setNom(String nom) {
         launica.nombre = nom;
     }
 
+    //Creación del método setApe
     public void setApe(String ape) {
         launica.apellido = ape;
     }
 
+    //Creación del método setEdad
     public void setEdad(int edad) {
         launica.edad = edad;
     }
 
+    //Generación del toString
     @Override
     public String toString() {
         return "Persona:" + "\nnombre = " + nombre + "\napellido = " +
                 apellido + "\nedad = " + edad+"\n";
     }
-
 
 }
--- src/singleton/Singleton.java
+++ src/singleton/Singleton.java
@@ -9,12 +9,17 @@ public class Singleton {
 
     public static void main(String[] args) {
 
+        //Creando el objeto miPersona
         Persona miPersona;
+        //Introduciendo los datos de la persona en el objeto miPersona
         miPersona = Persona.getInstancePar("Juan","Gutiérrez",36);
 
+        //Sacando por pantalla los datos del objeto miPersona
         System.out.println(miPersona.toString());
+        //Creación del objeto tuPersona
         Persona tuPersona = Persona.getInstance();
 
+        //Sacando por pantalla los datos de tuPersona y miPersona
         System.out.println(tuPersona.toString());
         System.out.println(miPersona.toString());
 
